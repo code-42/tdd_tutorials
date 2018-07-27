1d3b6620785da7c7698e2cefebd743559cd86bd0
Edward Dupre <edwd42@gmail.com>
2018-07-26 21:45:39 -0400

    add code to methods add() and subtract() to pass test

--- TDDwithJUnit/src/net/ed/learn/Matherator.java
+++ TDDwithJUnit/src/net/ed/learn/Matherator.java
@@ -1,17 +1,27 @@
 package net.ed.learn;
 
+// tutorial https://www.youtube.com/watch?v=2Ekty7t621k
+
 public class Matherator implements Adder, Subtracter {
 
 	@Override
 	public long add(long... operands) {
-		// TODO Auto-generated method stub
-		return 0;
+		
+	    long ret = 0;
+	    for (long operand : operands) {
+	      ret += operand;
+	    }
+	    return ret;
 	}
 
 	@Override
 	public long subtract(long... operands) {
-		// TODO Auto-generated method stub
-		return 0;
+
+	    long ret = operands[0];
+	    for (int aa = 1; aa < operands.length; aa++) {
+	      ret -= operands[aa];
+	    }
+	    return ret;
 	}
 
 }
---
+++ TDDwithJUnit/test/MatheratorTest.java
@@ -0,0 +1,46 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import net.ed.learn.Matherator;
+
+public class MatheratorTest {
+	
+	// add some scaffolding
+	private Matherator classUnderTest;
+
+	@Before
+	public void setUp() throws Exception {
+		classUnderTest = new Matherator();
+	}
+
+	@Test
+	public void testAdd() {
+
+		// let Java add it up
+		long result = 10 + 23 + (-1) + 16;
+		
+		// Compare that to what the Matherator gives us
+	    assertEquals(result, classUnderTest.add(10, 23, -1, 16));
+
+	    result = 100 + 102;
+	    assertEquals(result, classUnderTest.add(100 + 102));
+
+	    result = 0;
+	    assertEquals(result, classUnderTest.add(0));
+	}
+
+	@Test
+	public void testSubtract() {
+
+		long result = 100 - 34 - (-17) - 4;
+
+	    assertEquals(result, classUnderTest.subtract(100, 34, -17, 4));
+
+	    result = 0;
+
+	    assertEquals(result, classUnderTest.subtract(0, 0, 0, 0));
+	}
+
+}
