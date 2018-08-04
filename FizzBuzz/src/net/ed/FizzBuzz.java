1c5171a7bf5dd81bc689f8a86bb685bf87937229
Edward Dupre <edwd42@gmail.com>
2018-08-03 20:08:00 -0400

    initial commit FizzBuzz TDD

---
+++ FizzBuzz/src/net/ed/FizzBuzz.java
@@ -0,0 +1,5 @@
+package net.ed;
+
+public class FizzBuzz {
+
+}
---
+++ FizzBuzz/test/net/ed/FizzBuzzTest.java
@@ -0,0 +1,15 @@
+package net.ed;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class FizzBuzzTest {
+
+	@Test
+	public void testFizzBizz() {
+		fail("Not yet implemented");
+	}
+
+}
