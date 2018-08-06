9ec8ce53d9ee4d3d95acffd51b4f1229cf35dc93
Edward Dupre <edwd42@gmail.com>
2018-08-06 12:18:41 -0400

    implement method FindNumberOfOccurences()

--- 30DaysOfTDD/UnitTests/net/ed/StringUtils.java
+++ 30DaysOfTDD/UnitTests/net/ed/StringUtils.java
@@ -4,7 +4,13 @@ public class StringUtils {
 
 	public int FindNumberOfOccurences(String sentenceToScan, char characterToScanFor) {
 
-		return 2;
+        char[] stringToCheckAsCharacterArray = sentenceToScan.toCharArray();
+		
+		int numberOfOccurenes = 0, same = 0;
+		for (char letter : stringToCheckAsCharacterArray)
+			same = (letter == characterToScanFor) ? numberOfOccurenes++ : 0; 
+
+        return numberOfOccurenes;
 	}
 
 }
