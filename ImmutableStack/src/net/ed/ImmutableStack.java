9870e8f36f1e1529a266fa261e137008d5742703
Edward Dupre <edwd42@gmail.com>
2018-08-06 17:56:29 -0400

    add Interface IStack<T> and class ImmutableStack<T> with subclass EmptyStack<T>

---
+++ ImmutableStack/src/net/ed/ImmutableStack.java
@@ -0,0 +1,44 @@
+package net.ed;
+
+public class ImmutableStack<T> implements IStack<T> {
+	
+	private class EmptyStack implements IStack<T>{
+
+		@Override
+		public IStack<T> push(T value) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public IStack<T> pop() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public T peek() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+	}
+
+	@Override
+	public IStack<T> push(T value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IStack<T> pop() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T peek() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
