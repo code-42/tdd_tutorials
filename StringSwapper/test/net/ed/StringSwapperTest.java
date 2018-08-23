a8e033ad9dec751e4a7e9a2da74fbb31919b4ee9
Edward Dupre <edwd42@gmail.com>
2018-08-23 14:51:55 -0400

    add class StringSwapper and StringSwapperTest and fails like it should

---
+++ StringSwapper/test/net/ed/StringSwapperTest.java
@@ -0,0 +1,14 @@
+package net.ed;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StringSwapperTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
